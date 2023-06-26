a3459047bc89fa4621e218663878153142d93240
Ziqi Lin <ziqilin329@gmail.com>
2023-06-26 19:32:41 -0400

    create the structure

---
+++ Calendar_CLI/Log.py
@@ -0,0 +1,18 @@
+# File log
+
+import datetime
+
+def write_log(username: str, function_name: str) -> None():
+    """
+    Track the datetime, user's name, and activities.
+    All the logs should be written in inverse chronological order.
+    """
+
+    # get time, name, activity
+    time = datetime.datetime.now()
+
+    # parse existing file
+
+    # add new line on the top
+
+    return 
