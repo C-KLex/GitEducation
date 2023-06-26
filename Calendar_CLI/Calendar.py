3cc5e9fb6e76b04fc354dfc56e6861850896c16b
Hsuan <yhf@bu.edu>
2023-06-26 14:14:02 -0400

    add: get_week_of_day()

---
+++ Calendar_CLI/Calendar.py
@@ -0,0 +1,17 @@
+import datetime
+
+def get_week_of_day(year: int, month: int, day: int) -> int:
+    """
+    Return what week the date is with the year. Every 1/1 to 1/7 of the year will be considered as week 1.
+    args:
+        year: postive integer(AD only)
+        month: from 1 to 12
+        day: from 1 to 31(with exception of Feb)
+    """
+    date = datetime.date(year, month, day)
+    first_day_of_year = datetime.date(year, 1, 1)
+
+    # Calculate the number of days between the provided date and the first day of the year
+    delta = date - first_day_of_year
+    week_number = delta.days // 7 + 1
+    return week_number
