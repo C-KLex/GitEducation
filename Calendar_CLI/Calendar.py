2e6390cdef4557a497cf83fc2ce365136b558a49
Viet Tran <viethtran11@gmail.com>
2023-06-27 11:23:55 -0400

    feat: leap years BC

--- Calendar_CLI/Calendar.py
+++ Calendar_CLI/Calendar.py
@@ -8,10 +8,19 @@ def number_of_days(year, month):
     """
 
     # Returns the number of days in the month depending on the year and month. 
-    if (month == 2) and (((year%4 == 0) and (year%100 != 100)) or ((year%100 == 0) and (year%400 == 0))):
-        return 29
-    elif (month == 2):
-        return 28
+    if (month == 2):
+        if (year >= 8):
+            if ((year%4 == 0) and (year%100 != 100)) or ((year%100 == 0) and (year%400 == 0)):
+                return 29
+            else:
+                return 28
+        elif (year >= -45) and (year <= -9):
+            if (abs(year) % 3 == 0):
+                return 29
+            else:
+                return 28
+        else:
+            return 28
     elif (month == 1 or month == 3 or month == 5 or month == 7 or month == 9 or month == 10):
         return 31
     else:
