b0077265505198699cc133e49244fab0a9c0308a
Weirdojord <118554548+Weirdojord@users.noreply.github.com>
2023-06-27 12:24:08 -0400

    `number_of_leap_years` and `main` first PR (#7)

--- Calendar_CLI/Calendar.py
+++ Calendar_CLI/Calendar.py
@@ -8,6 +8,25 @@ Date: 20230626
 """
 import datetime
 
+
+def number_of_leap_years(year1: int, year2: int) -> int():
+    """
+    Return the number of leap years between year1, and year2, including year1 and year2 if there are also leap years.
+
+    Args:
+        year1 (int): the year to start counting leap year.  
+        year2 (int): the year to stop counting leap year.  
+    
+    Returns:
+        int: return the number of leap years appeared in this time interval.
+    """
+    count = 0
+    for year in range(year1, year2 + 1):
+        if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+            count += 1
+    return count
+
+
 def get_week_of_day(year: int, month: int, day: int) -> int():
     """
     Return what week the date is with the year. Every 1/1 to 1/7 of the year will be considered as week 1.
@@ -24,6 +43,6 @@ def get_week_of_day(year: int, month: int, day: int) -> int():
         delta = date - first_day_of_year
         week_number = delta.days // 7 + 1
         return week_number
-    
+
     except ValueError:
         return "Error: Invalid Date "
