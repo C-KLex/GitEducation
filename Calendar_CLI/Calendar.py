32bebf4a7d4898615d4cdb736ad85f323a86f89b
Viet Tran <viethtran11@gmail.com>
2023-06-28 15:25:21 -0400

    bug: conditional case logic for leap year

--- Calendar_CLI/Calendar.py
+++ Calendar_CLI/Calendar.py
@@ -20,7 +20,7 @@ def number_of_days(year, month):
     # Returns the number of days in the month depending on the year and month. 
     if (month == 2):
         if (year > 0):
-            if ((year % 4 == 0) and (year % 100 != 100)) or ((year % 100 == 0) and (year % 400 == 0)):
+            if ((year % 4 == 0) and (year % 100 != 0)) or ((year % 100 == 0) and (year % 400 == 0)):
                 return 29 
             else:
                 return 28
