9eb10fa215b0f2bd427dc7846456118b13ea3c82
viettran15 <63764786+viettran15@users.noreply.github.com>
2023-06-28 17:14:24 -0400

    `number_of_days()` second PR bug fix (#12)
    * fix: leap year calculation
    
    * bug: conditional case logic for leap year
    
    * bug: conditional case logic for leap year
    
    ---------
    
    Co-authored-by: Viet Tran <viethtran11@gmail.com>

--- Calendar_CLI/Calendar.py
+++ Calendar_CLI/Calendar.py
@@ -20,8 +20,8 @@ def number_of_days(year, month):
     # Returns the number of days in the month depending on the year and month. 
     if (month == 2):
         if (year > 0):
-            if ((year % 4 == 0) and (year % 100 != 100)) or ((year % 100 == 0) and (year % 400 == 0)):
-                return 29
+            if ((year % 4 == 0) and (year % 100 != 0)) or ((year % 100 == 0) and (year % 400 == 0)):
+                return 29 
             else:
                 return 28
         elif (year < 0):
@@ -31,7 +31,7 @@ def number_of_days(year, month):
                 return 28
         else: 
             raise ValueError("0 is not a valid year. Please enter either a positive or negative number, corresponding to BC or AD.")
-    elif (month == 1 or month == 3 or month == 5 or month == 7 or month == 9 or month == 10):
+    elif (month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
         return 31
     else:
         return 30
