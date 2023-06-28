b2fb568e18e30e1998bd718aeb4a60187f704686
Ziqi Lin <ziqilin329@gmail.com>
2023-06-28 15:59:32 -0400

    style: delete template

--- Calendar_CLI/Log.py
+++ Calendar_CLI/Log.py
@@ -10,12 +10,6 @@ Date: 20230626
 import datetime
 import pandas as pd
 
-df = pd.read_csv('Calendar_CLI/Log.csv', header = 'infer')
-df_new = pd.DataFrame([1, 2, 3]).T
-df_new.columns = df.columns
-df = pd.concat([df_new, df], axis=0, ignore_index=True)
-print(df)
-
 def write_log(username: str, function_name: str):
     """
     Track the datetime, user's name, and activities.
